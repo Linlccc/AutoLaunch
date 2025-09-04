01d0910376c55a82978ae885ca6bc006c0e3a104
Lin <linlccc@163.com>
2025-09-04 10:08:45 +0800

    Filter permission errors in MacOSAppleScript and throw PermissionDeniedException

--- src/AutoLaunch/Platforms/MacOSAppleScript.cs
+++ src/AutoLaunch/Platforms/MacOSAppleScript.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace AutoLaunch.Platforms;
 
 #if NET5_0_OR_GREATER
@@ -10,39 +12,20 @@ internal sealed partial class MacOSAppleScript(string appName, string appPath, R
     public override bool IsEnabled() => string.Equals("true", Exec(GetIsEnabledScript()), StringComparison.OrdinalIgnoreCase);
 
     #region private method
-    private string GetEnableScript()
-    {
-        bool hidden = args.Any(arg => arg is "--hidden" or "--minimized");
-        return SystemEventsTo($$"""make login item at end with properties {name:"{{appName}}",path:"{{appPath}}",hidden:{{hidden.ToString().ToLower()}}}""");
-    }
+    private static string Exec(string script) => ProcessResult(ProcessEx.Start("osascript", "-e", script), script);
+    private static async Task<string> ExecAsync(string script) => ProcessResult(await ProcessEx.StartAsync("osascript", "-e", script), script);
+
+    private string GetEnableScript() => SystemEventsTo($$"""make login item at end with properties {name:"{{appName}}",path:"{{appPath}}",hidden:{{args.Any(arg => arg is "--hidden" or "--minimized").ToString().ToLower()}}}""");
     private string GetDisableScript() => SystemEventsTo($"delete login item \"{appName}\"");
     private string GetIsEnabledScript() => SystemEventsTo($"return exists login item \"{appName}\"");
-
     private static string SystemEventsTo(string scriptSuffix) => $"""tell application "System Events" to {scriptSuffix}""";
 
-    private static string Exec(string script)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ProcessResult(ProcessResult pres, string script)
     {
-        try
-        {
-            ProcessResult execRes = ProcessEx.Start("osascript", "-e", script);
-            return execRes.ExitCode == 0 ? execRes.Output.Trim() : throw new ExecuteCommandException(script, execRes.ExitCode, execRes.Error);
-        }
-        catch (Exception ex) when (ex is not ExecuteCommandException)
-        {
-            throw new ExecuteCommandException(ex);
-        }
-    }
-    private static async Task<string> ExecAsync(string script)
-    {
-        try
-        {
-            ProcessResult execRes = await ProcessEx.StartAsync("osascript", "-e", script);
-            return execRes.ExitCode == 0 ? execRes.Output.Trim() : throw new ExecuteCommandException(script, execRes.ExitCode, execRes.Error);
-        }
-        catch (Exception ex) when (ex is not ExecuteCommandException)
-        {
-            throw new ExecuteCommandException(ex);
-        }
+        if (pres.ExitCode == 0) return pres.Output.Trim();
+        if (pres.Error.Contains("-1743")) throw new PermissionDeniedException("Permission denied. (-1743)");
+        throw new ExecuteCommandException(script, pres.ExitCode, pres.Error);
     }
     #endregion
 }
