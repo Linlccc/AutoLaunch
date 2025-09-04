df6101530fdcb410593829853ef91565b7a8b3d3
Lin <linlccc@163.com>
2025-09-04 11:45:03 +0800

    Filter permission errors in WindowsStartupFolder and throw PermissionDeniedException

--- src/AutoLaunch/Platforms/WindowsStartupFolder.cs
+++ src/AutoLaunch/Platforms/WindowsStartupFolder.cs
@@ -8,16 +8,16 @@ internal sealed partial class WindowsStartupFolder(string appName, string appPat
     private readonly string _useStartupFolder = workScope == WorkScope.CurrentUser ? Environment.GetFolderPath(Environment.SpecialFolder.Startup) : Environment.GetFolderPath(Environment.SpecialFolder.CommonStartup);
     private string BatchFilePath => Path.Combine(_useStartupFolder, $"{appName}.bat");
 
-    public override void Enable()
+    public override void Enable() => PermissionDeniedException.ThrowIfIOPermissionDenied(() =>
     {
         if (!Directory.Exists(_useStartupFolder)) Directory.CreateDirectory(_useStartupFolder);
         File.WriteAllText(BatchFilePath, GetBatchFileContent());
-    }
-    public override void Disable()
+    });
+    public override void Disable() => PermissionDeniedException.ThrowIfIOPermissionDenied(() =>
     {
         if (File.Exists(BatchFilePath)) File.Delete(BatchFilePath);
-    }
-    public override bool IsEnabled() => File.Exists(BatchFilePath);
+    });
+    public override bool IsEnabled() => PermissionDeniedException.ThrowIfIOPermissionDenied(() => File.Exists(BatchFilePath));
 
     #region private method
     private string GetBatchFileContent() => $"""
@@ -33,15 +33,15 @@ internal sealed partial class WindowsStartupFolder(string appName, string appPat
 
 internal sealed partial class WindowsStartupFolder
 {
-    public override Task EnableAsync()
+    public override Task EnableAsync() => PermissionDeniedException.ThrowIfIOPermissionDeniedAsync(() =>
     {
         if (!Directory.Exists(_useStartupFolder)) Directory.CreateDirectory(_useStartupFolder);
         return FileEx.WriteAllTextAsync(BatchFilePath, GetBatchFileContent());
-    }
-    public override Task DisableAsync()
+    });
+    public override Task DisableAsync() => PermissionDeniedException.ThrowIfIOPermissionDeniedAsync(() =>
     {
         if (File.Exists(BatchFilePath)) File.Delete(BatchFilePath);
         return Task.CompletedTask;
-    }
-    public override Task<bool> IsEnabledAsync() => Task.FromResult(File.Exists(BatchFilePath));
+    });
+    public override Task<bool> IsEnabledAsync() => PermissionDeniedException.ThrowIfIOPermissionDeniedAsync(() => Task.FromResult(File.Exists(BatchFilePath)));
 }
