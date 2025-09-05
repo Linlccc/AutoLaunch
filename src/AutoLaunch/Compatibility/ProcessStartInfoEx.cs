24a0b8ba4317fa02c896c6dc6295c4dca957e469
Lin <linlccc@163.com>
2025-09-05 09:30:10 +0800

    Remove redundant conversions in ProcessStartInfoEx

--- src/AutoLaunch/Compatibility/ProcessStartInfoEx.cs
+++ src/AutoLaunch/Compatibility/ProcessStartInfoEx.cs
@@ -5,7 +5,7 @@ internal static class ProcessStartInfoEx
     internal static ProcessStartInfo New(string fileName, params IEnumerable<string> arguments)
     {
 #if NETSTANDARD2_0_OR_GREATER
-        return new ProcessStartInfo(fileName, ArgumentEx.EscapeArguments(arguments.ToArray()));
+        return new ProcessStartInfo(fileName, ArgumentEx.EscapeArguments(arguments));
 #else
         return new ProcessStartInfo(fileName, arguments);
 #endif
